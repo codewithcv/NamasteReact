c5c39792b0f544207e9d56f2f69bc30375b80b43
codewithcv <vignesh.aex@gmail.com>
2023-03-26 16:09:24 +0530

    Hello from React

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading1 = React.createElement("h1", { id: "head" }, [
+  React.createElement("div", { id: "child1" }, "Hello from React h1 child1"),
+  React.createElement("div", { id: "child2" }, "Hello from React h1 child2"),
+]);
+const root1 = ReactDOM.createRoot(document.getElementById("reactRoot"));
+
+root1.render(heading1);
