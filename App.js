e669f8c74e6992a4ab9a0a49884115801e1c11ad
codewithcv <vignesh.aex@gmail.com>
2023-04-12 08:35:18 +0530

    JSX introduced and tried multiple fragments and multiple render inside html

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "head" }, [
+const heading = React.createElement("h1", { id: "head" }, [
   React.createElement("div", { id: "child1" }, "Hello from React h1 child1"),
   React.createElement("div", { id: "child2" }, "Hello from React h1 child2"),
 ]);
-const root1 = ReactDOM.createRoot(document.getElementById("reactRoot"));
 
-root1.render(heading1);
+//React Component
+const Title = () => {
+  return <h1>Hello From Component</h1>;
+};
+
+//React Element
+const element = <h1>This is an element</h1>;
+
+//React Element contains component and eleemnt
+const jsxHeading = (
+  <>
+    <></>
+    {element}
+    <h1 id="jsxID" className="jsxClass">
+      Namaste react from JSX!!
+    </h1>
+    <Title />
+  </>
+);
+
+const childElement = <h1>Hi I'm child element</h1>;
+
+const root1 = ReactDOM.createRoot(document.getElementById("reactRoot"));
+const root2 = ReactDOM.createRoot(document.getElementById("reactChildRoot"));
+root1.render(jsxHeading);
+root2.render(childElement);
