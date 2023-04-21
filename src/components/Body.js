8e5e362f9b464cd7ce3f234eb18da9ae17183367
codewithcv <vignesh.aex@gmail.com>
2023-04-22 04:57:02 +0530

    Added Search, No Results, Show all and Show filtered restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,57 @@
 import RestCards from "./RestCards";
 import { restaurantData } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(restaurantData);
+  const [searchRestaurant, setSearchRestaurant] = useState("");
   return (
     <div id="body-container">
-      <div id="search">Search Box</div>
+      <input
+        type="text"
+        placeholder="Search Restaurants"
+        value={searchRestaurant}
+        onChange={(e) => {
+          let searchText = e.target.value;
+          setSearchRestaurant(searchText);
+          const searchedRestaurant = restaurantData.filter((restuarant) => {
+            return restuarant.data.name
+              .toLowerCase()
+              .includes(searchText.toLowerCase());
+          });
+          setRestaurantList(searchedRestaurant);
+        }}
+      ></input>
+      <button
+        className="top-restaurants"
+        onClick={() => {
+          const filteredRestaurant = restaurantList.filter((restaurant) => {
+            return restaurant.data.avgRating > 4.0;
+          });
+          setRestaurantList(filteredRestaurant);
+        }}
+      >
+        Filter Top Restaurant
+      </button>
+      <button
+        className="all-restaurants"
+        onClick={() => {
+          setRestaurantList(restaurantData);
+        }}
+      >
+        All Restaurants
+      </button>
       <div id="res-cards-container">
-        {restaurantData.map((restaurant) => (
-          <RestCards
-            key={restaurant.data.id}
-            reslist={restaurant.data}
-          ></RestCards>
-        ))}
+        {restaurantList.length > 0 ? (
+          restaurantList.map((restaurant) => (
+            <RestCards
+              key={restaurant.data.id}
+              reslist={restaurant.data}
+            ></RestCards>
+          ))
+        ) : (
+          <h1>No Results found</h1>
+        )}
       </div>
     </div>
   );
