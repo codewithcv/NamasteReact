1bce9ef4c54127dd8d4cdb21d06c69f358a57091
codewithcv <vignesh.aex@gmail.com>
2023-04-22 05:40:47 +0530

    Moved functions to top

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,22 @@ import RestCards from "./RestCards";
 import { restaurantData } from "../utils/mockData";
 import { useState } from "react";
 
+const topRestaurants = (listOfRestaurant) => {
+  const filteredRestuarant = listOfRestaurant?.filter((restaurant) => {
+    return restaurant.data.avgRating > 4.0;
+  });
+  return filteredRestuarant;
+};
+
+const searchedRestaurant = (searchText, listOfRestaurant) => {
+  const filteredRestuarant = listOfRestaurant?.filter((restuarant) => {
+    return restuarant.data.name
+      .toLowerCase()
+      .includes(searchText.toLowerCase());
+  });
+  return filteredRestuarant;
+};
+
 const Body = () => {
   const [restaurantList, setRestaurantList] = useState(restaurantData);
   const [searchRestaurant, setSearchRestaurant] = useState("");
@@ -14,24 +30,21 @@ const Body = () => {
         onChange={(e) => {
           let searchText = e.target.value;
           setSearchRestaurant(searchText);
-          const searchedRestaurant = restaurantData.filter((restuarant) => {
-            return restuarant.data.name
-              .toLowerCase()
-              .includes(searchText.toLowerCase());
-          });
-          setRestaurantList(searchedRestaurant);
+          const searchedRestaurantList = searchedRestaurant(
+            searchText,
+            restaurantData
+          );
+          setRestaurantList(searchedRestaurantList);
         }}
       ></input>
       <button
         className="top-restaurants"
         onClick={() => {
-          const filteredRestaurant = restaurantList.filter((restaurant) => {
-            return restaurant.data.avgRating > 4.0;
-          });
-          setRestaurantList(filteredRestaurant);
+          const topRestaurantsList = topRestaurants(restaurantList);
+          setRestaurantList(topRestaurantsList);
         }}
       >
-        Filter Top Restaurant
+        Show Top Restaurants
       </button>
       <button
         className="all-restaurants"
@@ -39,10 +52,10 @@ const Body = () => {
           setRestaurantList(restaurantData);
         }}
       >
-        All Restaurants
+        Show All Restaurants
       </button>
       <div id="res-cards-container">
-        {restaurantList.length > 0 ? (
+        {restaurantList?.length > 0 ? (
           restaurantList.map((restaurant) => (
             <RestCards
               key={restaurant.data.id}
